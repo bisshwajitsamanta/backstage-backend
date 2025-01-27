2ee336f39c59ff005a9d1d154014e460e75351e1
Bisshwajit Samanta <bisshwajitsamanta@gmail.com>
2025-01-27 19:29:22 +0530

    Updated Frontend.py and html form

--- frontend.py
+++ frontend.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('form.html')
 
-@app.route('/submit')
+@app.route('/submit', methods=['POST'])
 def submit():
     selected_time = request.form.get('time_of_day')
     return redirect(url_for('display_greeting',time_of_day=selected_time))
+
+@app.route('/greet')
+def display_greeting():
+    time_of_day = request.args.get('time_of_day','Morning')
+    return f"Good {time_of_day.capitalize()}! This Page is work in Progress."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
